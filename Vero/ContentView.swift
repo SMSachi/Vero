86bb3fa2b204400150ef15d1fd7d1d9c9dd663c2
SMSachi <sachi.shah@utexas.edu>
2026-03-15 17:20:34 -0500

    Initial Commit

---
+++ Vero/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Vero
+//
+//  Created by Sachi Shah on 3/15/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Vero/VeroApp.swift
@@ -0,0 +1,17 @@
+//
+//  VeroApp.swift
+//  Vero
+//
+//  Created by Sachi Shah on 3/15/26.
+//
+
+import SwiftUI
+
+@main
+struct VeroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
